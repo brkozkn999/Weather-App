42cd5468e9d4b7b2909a556958a2d26156f1473a
brkozkn999 <ibrahimberkozkan34@gmail.com>
2024-05-22 20:21:32 +0300

    deploy

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Istanbul",
+    country: "TR",
+    state: "Istanbul",
+    lat: 41.0082,
+    lon: 28.9784
+  },
+  {
+    name: "New York",
+    country: "US",
+    state: "New York",
+    lat: 40.7128,
+    lon: -74.006,
+  },
+  {
+    name: "London",
+    country: "GB",
+    state: "England",
+    lat: 51.5074,
+    lon: 0.1278,
+  },
+  {
+    name: "Barcelona",
+    country: "ES",
+    state: "Catalonia",
+    lat: 41.3828939,
+    lon: 2.1774322,
+  },
+  {
+    name: "Madrid",
+    country: "ES",
+    state: "Madrid",
+    lat: 40.4165,
+    lon: -3.7026,
+  },
+];
+
+export default defaultStates;
